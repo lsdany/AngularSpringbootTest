835455c8dab4cc316fdb0665404452de4a459c74
luisdany Pernillo <Luisdany.Pernillo@telus.com>
2019-11-15 18:51:51 -0600

    Initial commit

---
+++ src/main/java/com/ld/angulartest/AngulartestApplication.java
@@ -0,0 +1,13 @@
+package com.ld.angulartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngulartestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngulartestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ld/angulartest/AngulartestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld.angulartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngulartestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
